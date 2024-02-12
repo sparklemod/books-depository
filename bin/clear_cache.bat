212f9474f24e1a828aca760de66b0315a31d97c7
EmEE <katerina5314@mail.ru>
2024-02-13 00:23:16 +0300

    fixture

--- bin/clear_cache.bat
+++
@@ -1,3 +0,0 @@
-php %cd%\bin\doctrine orm:clear-cache:result
-php %cd%\bin\doctrine orm:clear-cache:query
-php %cd%\bin\doctrine orm:clear-cache:metadata
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Author;
+use App\Entity\Book;
+use App\Entity\Publisher;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+
+        for ($i = 0; $i < 5; $i++) {
+            $author = new Author();
+            $author->setName('author'.$i)
+            ->setSurname('surname'.$i);
+
+            $publisher = new Publisher();
+            $publisher->setName('publisher'.$i)
+            ->setAddress('address'.$i);
+
+            $book = new Book();
+            $year = new \DateTime();
+            $year->modify(-$i . 'year')->format('Y');
+            $book->setTitle('book'.$i)
+            ->setYear($year)
+            ->setPublisherId($publisher)
+            ->addAuthor($author);
+
+            $author->addBook($book);
+            $publisher->addBookId($book);
+
+            $manager->persist($author);
+            $manager->persist($publisher);
+            $manager->persist($book);
+        }
+
+        $manager->flush();
+    }
+}
